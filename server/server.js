405a941039ed8a76242145f5ee6cb2c3e372eeb3
Ksenia Smith <smithkseniav@gmail.com>
2016-03-02 13:59:33 -0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var env = require('../.env');
+
+app.use(express.static(__dirname + '/client'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("Listening on port 3000");
